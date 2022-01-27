87a13898d9171106d8e5c48e9eafe833f8d81f0f
Diego Osvaldo <g11ada18@hackademy.mx>
2022-01-26 23:33:12 -0400

    user serializer and update profile

--- user/serializers.py
+++ user/serializers.py
@@ -15,6 +15,17 @@ class UserSerializer(serializers.ModelSerializer):
         # crear nuevo usuario con clave encriptada
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        # actualiza usuario, configura el password correctamente
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     # Serializador para el objeto de autenticacion de usuarios
--- user/tests/test_user_api.py
+++ user/tests/test_user_api.py
@@ -7,6 +7,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**params):
@@ -79,3 +80,38 @@ class PublicUserApiTest(TestCase):
         res = self.client.post(TOKEN_URL, {'email': 'one', 'password': ''})
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        # prueba que la autenticacion sea requerida
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTest(TestCase):
+    # testear el API Privado del usuario
+
+    def setUp(self):
+        self.user = create_user(email='test@email.com', password='pass123', name='name user')
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        # probar obtener perdil para usuario con login
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {'name': self.user.name, 'email': self.user.email})
+
+    def test_post_me_not_allowed(self):
+        # prueba que el post no sea permitido
+        res = self.client.post(ME_URL, {})
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        # probar que el usuario esta siendo actualizado si esta autenticado
+        payload = {'name': 'new name', 'password': 'newpass'}
+
+        res = self.client.patch(ME_URL, payload)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)

55605703a68e4c41582eb6179c5c257738545324
Diego Osvaldo <g11ada18@hackademy.mx>
2022-01-28 02:13:32 -0400

    filter tags for users and recipes

--- recipe/tests/test_tags_api.py
+++ recipe/tests/test_tags_api.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
-from core.models import Tag
+from core.models import Tag, Recipe
 from recipe.serializers import TagSerializer
 
 TAGS_URL = reverse('recipe:tag-list')
@@ -65,8 +65,32 @@ class PrivateTagsApiTests(TestCase):
 
     def test_create_tag_invalid(self):
         # prueba crear nuevo tag invalido
-        payload = {'name':''}
+        payload = {'name': ''}
         res = self.client.post(TAGS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-    
+
+    def test_retrieve_tags_assigned_to_recipe(self):
+        # probar filtro de tags basados en recetas
+        tag1 = Tag.objects.create(user=self.user, name='Meat')
+        tag2 = Tag.objects.create(user=self.user, name='Banana')
+        recipe = Recipe.objects.create(title='Test recipe', time_minutes=13, price=12.50, user=self.user)
+        recipe.tags.add(tag1)
+
+        res = self.client.get(TAGS_URL, {'assigned_only': 1})
+
+        serializer1 = TagSerializer(tag1)
+        serializer2 = TagSerializer(tag2)
+        self.assertIn(serializer1.data, res.data)
+        self.assertNotIn(serializer2.data, res.data)
+
+    def test_retrieve_tags_assigned_unique(self):
+        # probar filtro de tags asignados por items unicos
+        tag = Tag.objects.create(user=self.user, name='Meat')
+        recipe1 = Recipe.objects.create(title='Test bresk', time_minutes=15, price=12.50, user=self.user)
+        recipe1.tags.add(tag)
+        recipe2 = Recipe.objects.create(title='Test fast', time_minutes=30, price=20.50, user=self.user)
+        recipe2.tags.add(tag)
+
+        res = self.client.get(TAGS_URL, {'assigned_only': 1})
+        self.assertEqual(len(res.data), 1)
